c2b717e6eb725819bbdafc73a315d28a177fef0c
thanshita <thanshitabharathi.m2021@vitstudent.ac.in>
2023-10-14 14:42:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { SketchPicker } from 'react-color';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('#000000');
+
+  const handleColorChange = (newColor) => {
+    setColor(newColor.hex);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Color Picker</h2>
+      <SketchPicker color={color} onChange={handleColorChange} />
+      <div>
+        Selected Color: <span style={{ color: color }}>{color}</span>
+      </div>
     </div>
   );
 }
